264da6c9fdcc5d73f71faa7d4563c8ba92095ead
tnanhekhan <1631995@gmail.com>
2021-01-24 02:48:30 +0100

    add mockito + unit tests for getting subreddit

--- app/src/test/java/me/ccrama/redditslide/test/UserSubscriptionsTest.java
+++ app/src/test/java/me/ccrama/redditslide/test/UserSubscriptionsTest.java
@@ -1,7 +1,15 @@
 package me.ccrama.redditslide.test;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,12 +19,18 @@ import me.ccrama.redditslide.UserSubscriptions;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 /**
  * Created by Alex Macleod on 28/03/2016.
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({UserSubscriptions.class})
 public class UserSubscriptionsTest {
     private final CaseInsensitiveArrayList subreddits = new CaseInsensitiveArrayList(Arrays.asList(
             "xyy", "xyz", "frontpage", "mod", "friends", "random", "aaa", "pinned", "pinned2"
@@ -39,5 +53,38 @@ public class UserSubscriptionsTest {
         assertThat(UserSubscriptions.sortNoExtras(subreddits), is(new ArrayList<>(Arrays.asList(
                 "pinned", "pinned2", "frontpage", "random", "friends", "mod", "aaa", "xyy", "xyz"
         ))));
+    }
+
+    @Test
+    public void getAllSubreddits() {
+        Context mockedContext = mock(Context.class);
+        SharedPreferences mockedSharedPreferences = mock(SharedPreferences.class);
+        UserSubscriptions.pinned = new TestUtils.MockPreferences("pinned,pinned2");
+
+        PowerMockito.mockStatic(UserSubscriptions.class);
+
+        when(mockedContext.getSharedPreferences(anyString(), anyInt())).thenReturn(mockedSharedPreferences);
+        BDDMockito.given(UserSubscriptions.getHistory()).willReturn(subreddits);
+        BDDMockito.given(UserSubscriptions.getDefaults(mockedContext)).willReturn(subreddits);
+        BDDMockito.given(UserSubscriptions.getSubscriptions(mockedContext)).willReturn(subreddits);
+
+        BDDMockito.given(UserSubscriptions.getAllSubreddits(mockedContext)).willCallRealMethod();
+        CaseInsensitiveArrayList result = UserSubscriptions.getAllSubreddits(mockedContext);
+        assert (!result.isEmpty());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void getAllSubredditsWithMissingDependencies() {
+        Context mockedContext = mock(Context.class);
+        SharedPreferences mockedSharedPreferences = mock(SharedPreferences.class);
+        UserSubscriptions.pinned = new TestUtils.MockPreferences("pinned,pinned2");
+
+        PowerMockito.mockStatic(UserSubscriptions.class);
+
+        when(mockedContext.getSharedPreferences(anyString(), anyInt())).thenReturn(mockedSharedPreferences);
+
+        BDDMockito.given(UserSubscriptions.getAllSubreddits(mockedContext)).willCallRealMethod();
+        CaseInsensitiveArrayList result = UserSubscriptions.getAllSubreddits(mockedContext);
+        assert (!result.isEmpty());
     }
 }
